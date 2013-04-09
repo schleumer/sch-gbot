f1c747e09351f0c579d90a3092046618381fae78
Wesley Schleumer <wesley.schleumer@gmail.com>
2013-04-09 15:30:07 -0300

    coisas

--- compile.sh
+++ compile.sh
@@ -1,7 +1,15 @@
-rm cbot
+PROJECT_NAME="gbot"
+PROJECT_OUTPUT="gbot"
+if [ -f "$PROJECT_OUTPUT" ]
+then
+    rm "$PROJECT_OUTPUT"
+fi
 gcc \
     -lcurl \
     -lpthread \
     -lstdc++ \
-    main.cpp -o cbot
-./cbot
+    main.cpp -o "$PROJECT_OUTPUT"
+if [ -f "$PROJECT_OUTPUT" ]
+then
+    ./"$PROJECT_OUTPUT"
+fi
